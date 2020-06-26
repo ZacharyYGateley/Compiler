079cadb10b5a431e73e566e0759d6061c085058d
zach <zygateley@gmail.com>
2020-06-25 20:39:42 -0500

    Parse trees successfully built

--- src/main/java/com/zygateley/compiler/Parser.java
+++ src/main/java/com/zygateley/compiler/Parser.java
@@ -5,15 +5,33 @@ import java.util.*;
 
 class ParseNode {
 	private final NonTerminal rule;
+	private final Terminal token;
+	private final Symbol symbol;
 	private final ArrayList<ParseNode> param;
 	
 	public ParseNode(NonTerminal rule) {
 		this.rule = rule;
+		this.token = null;
+		this.symbol = null;
+		this.param = new ArrayList<ParseNode>();
+	}
+	public ParseNode(Terminal token, Symbol symbol) {
+		this.rule = null;
+		this.token = token;
+		this.symbol = symbol;
 		this.param = new ArrayList<ParseNode>();
 	}
 
 	public NonTerminal getRule() {
 		return this.rule;
+	}
+	
+	public Terminal getToken() {
+		return this.token;
+	}
+	
+	public Symbol getSymbol() {
+		return this.symbol;
 	}
 	
 	public ArrayList<ParseNode> getParam() {
@@ -41,7 +59,7 @@ public class Parser {
 		this.parseTree = new ArrayList<ParseNode>();
 	}
 	
-	public String parse() {
+	public ArrayList<ParseNode> parse() {
 	    // Get token and consume stream until stream is empty
 	    NEXT_TOKEN: while (tokenStream.length() > 0) {
 	        // Peek next token
@@ -50,33 +68,72 @@ public class Parser {
 	         
 	        // Find first applicable rule
 	        for (NonTerminal rule : NonTerminal.values()) {
-	        	boolean isThisRule = rule.inFirst(t);
+	        	int indexInFirst = rule.indexInFirst(t);
+	        	boolean isThisRule = indexInFirst > -1;
 	        	
 	        	// MATCH!
 	        	if (isThisRule) {
-	        		// Set this rule as the current rule in the parser
-	        		this.rule.push(rule);
-	        		
-	        		// Create new top-level ParseNode for this statement
-	        		ParseNode pn = new ParseNode(rule);
-	        		// Get all parameters for this node (if not a terminal)
-	        		
-	        		
-	        		// When finished with token, 
-	        		// if this is a top-level statement, 
-	        		// add this parse node to the parse tree
-	        		if (this.rule.pop() != rule) {
-	        			System.err.println("Fatal error: Bad rule embedding.");
-	        		}
-	        		if (this.rule.size() == 0) {
+	        		// Create parse tree at this top-level rule
+	        		// Starting at this location in the token stream
+	        		ParseNode pn = parseRule(rule);
+	        		if (pn != null) {
 	        			this.parseTree.add(pn);
 	        		}
+	        		
 	        		// When match is found and processed, keep going
 	        		continue NEXT_TOKEN;
 	        	}
 	        }
+	        
+	        // No match
+	        // Invalid syntax
+	        System.err.println("Syntax error: Rule not found starting with " + t);
+	        tokenStream.gettoken();
 	    }
 	     
-	    return output.toString();
+	    return this.parseTree;
+	}
+	
+	private ParseNode parseRule(NonTerminal rule) {
+		ParseNode pn = new ParseNode(rule);
+		
+		Terminal t = tokenStream.peek().token;
+
+		// Get pattern to work with
+    	int indexInFirst = rule.indexInFirst(t);
+    	if (indexInFirst < 0) {
+    		System.err.println("Fatal error: Production Rule terminated prematurely.");
+    		return null;
+    	}
+		int[] pattern = rule.patterns[indexInFirst].PATTERN;
+		
+		// Get rules in order
+		// If any of the rules is a NonTerminal, recur
+		for (int tokenValue : pattern) {
+			ParseNode next;
+			// Terminal
+			// These are immediately added to the parse tree
+			if (Token.isTerminal(tokenValue)) {
+				TokenSymbol ts = tokenStream.gettoken();
+				if (ts.token.tokenValue != tokenValue) {
+					System.err.println("Fatal error: incorrect syntax.");
+					return null;
+				}
+				next = new ParseNode(ts.token, ts.symbol);
+			}
+			// NonTerminals
+			// Recur into parseRule
+			else {
+				next = parseRule(NonTerminal.getNonTerminal(tokenValue));
+				if (next == null) {
+					return null;
+				}
+			}
+			
+			// Add resulting token or tree as parameter to this rule
+			pn.addParam(next);
+		}
+		
+		return pn;
 	}
 }
